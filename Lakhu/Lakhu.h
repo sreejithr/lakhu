4c4cc042636811705b2c43c43613b0016b66500a
Sreejith <sreejith.r44@gmail.com>
2015-04-10 18:05:48 +0530

    Initial Commit

---
+++ Lakhu/Lakhu.h
@@ -0,0 +1,13 @@
+//
+//  Lakhu.h
+//  Lakhu
+//
+//  Created by Sreejith on 10/04/15.
+//  Copyright (c) 2015 Sreejith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lakhu : NSObject
+
+@end
---
+++ Lakhu/Lakhu.m
@@ -0,0 +1,13 @@
+//
+//  Lakhu.m
+//  Lakhu
+//
+//  Created by Sreejith on 10/04/15.
+//  Copyright (c) 2015 Sreejith. All rights reserved.
+//
+
+#import "Lakhu.h"
+
+@implementation Lakhu
+
+@end
