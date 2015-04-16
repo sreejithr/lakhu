2bbf72770737d49141b793cd4764cc48fc2c5fc4
Sreejith <sreejith.r44@gmail.com>
2015-04-16 15:00:01 +0530

    Assigned this to self. Also added statement type detection which will send it to the corresponding native method

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ var Q = require('q');
 var Native = require('NativeModules');
 
 function Lakhu() {
-  var that = this;
-  this._conn = Native.Lakhu;
+  var self = this;
+  this.db = Native.Lakhu;
   return this;
 };
 
 Lakhu.prototype.connect = function(config) {
   var filename = config['filename'];
-  return Q.nfcall(this._conn.connect, filename);
+  return Q.nfcall(self.db.connect, filename);
 };
 
 Lakhu.prototype.query = function(query) {
-  return Q.nfcall(this._conn.executeSelectQuery, query);
+  for (var cmd in query.split(' ')) {
+    switch (cmd.toLowerCase()) {
+    case 'select':
+      return Q.nfcall(self.db.executeSelectQuery, query);
+
+    case 'insert':
+      return Q.nfcall(self.db.executeUpdateQuery, query);
+
+    case 'delete':
+      return Q.nfcall(self.db.executeUpdateQuery, query);
+    }
+  }
+  return Q.fcall(() => (new Error("Invalid query statement"), null));
 };
 
 module.exports = Lakhu;
